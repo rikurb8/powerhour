a042f9af1fcdcb059d549739464082dbaeaf38ba
Riku Marttila <riku.marttila@vincit.com>
2020-04-16 22:48:16 +0300

    Add fixed Jitsi Meet room

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-
-import Powerhour from './Powerhour';
+import { Jutsu } from 'react-jutsu';
+import generateName from 'sillyname';
 
 import logo from './powerhourlogo.png';
 import './App.css';
 
+const DEFAULT_ROOM_NAME = 'SECRET_ROOM_666_NO_PASSWORD';
+
 function App() {
+  const name = generateName();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <img src={logo} className="logo" alt="logo" />
+      <div className="jitsi">
+        <Jutsu
+          roomName={DEFAULT_ROOM_NAME}
+          userName={name}
+          loadingComponent={<p>LOADING....</p>}
+        />
+      </div>
     </div>
   );
 }
--- src/Powerhour.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-function Powerhour() {}
-
-export default Powerhour;
