2bcd1503cffb91ffa25c94a88d684f707d58f59c
Riku Marttila <riku.marttila@vincit.com>
2020-04-17 00:35:53 +0300

    Cooler clock hand

--- src/Clock.js
+++ src/Clock.js
@@ -1,23 +1,10 @@
 import React from 'react';
+import arrow from './arrow.png';
 
 function Clock({ show }) {
   return (
-    <div className="clock-container">
-      {show ? (
-        <React.Fragment>
-          <div class="line"></div>
-          <div class="circle0 circle"></div>
-
-          <div class="circle1 circle"></div>
-          <div class="circle2 circle"></div>
-          <div class="circle3 circle"></div>
-          <div class="circle4 circle"></div>
-          <div class="circle5 circle"></div>
-          <div class="circle6 circle"></div>
-        </React.Fragment>
-      ) : (
-        <h1>Get ready, next round starting soon...</h1>
-      )}
+    <div className="clock-container rotate">
+      <img src={arrow} className="clock-hand" />
     </div>
   );
 }
